660a6bbaa076d7c6b3d4655c38d9731286d41489
Mick McCarthy <mccarthy.mick0012@gmail.com>
2019-08-05 21:16:22 +0100

    Returning objects based on route params

--- index.js
+++ index.js
@@ -1,17 +1,25 @@
 const express = require('express');
 const app = express();
 
+const courses = [
+    { id: 1, name: 'Course 1'},
+    { id: 2, name: 'Course 2'},
+    { id: 3, name: 'Course 3'},
+    { id: 4, name: 'Course 4'},
+]
+
 app.get('/', (req, res) => {
     res.send('Hello world');
 });
 
 app.get('/api/courses', (req, res) => {
-    res.send([1, 2, 3]);
+    res.send(courses);
 });
 
 app.get('/api/courses/:id', (req, res) => {
-    res.send(req.params.id);
-    // res.send(req.query);
+    const course = courses.find(c => c.id === parseInt(req.params.id));
+    if (!course) res.status(404).send(`A course with id '${req.params.id}' could not be found`);
+    res.send(course);
 });
 
 const port = process.env.PORT || 3000;
