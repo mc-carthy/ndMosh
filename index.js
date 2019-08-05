01a8be0972e2f78ac4f902b599814b80124dd06b
Mick McCarthy <mccarthy.mick0012@gmail.com>
2019-08-05 21:32:03 +0100

    Creating new courses with POST endpoint

--- index.js
+++ index.js
@@ -8,6 +8,8 @@ const courses = [
     { id: 4, name: 'Course 4'},
 ]
 
+app.use(express.json());
+
 app.get('/', (req, res) => {
     res.send('Hello world');
 });
@@ -20,6 +22,15 @@ app.get('/api/courses/:id', (req, res) => {
     const course = courses.find(c => c.id === parseInt(req.params.id));
     if (!course) res.status(404).send(`A course with id '${req.params.id}' could not be found`);
     res.send(course);
+});
+
+app.post('/api/courses', (req, res) => {
+    const course = {
+        id: courses.length + 1,
+        name: req.body.name
+    };
+    courses.push(course);
+    res.status(201).send(course);
 });
 
 const port = process.env.PORT || 3000;
