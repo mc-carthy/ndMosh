8d445ef3110dd2e83c6c4e35a3d6bf3fc108f335
Mick McCarthy <mccarthy.mick0012@gmail.com>
2019-08-05 21:44:01 +0100

    Basic input validation

--- index.js
+++ index.js
@@ -25,6 +25,10 @@ app.get('/api/courses/:id', (req, res) => {
 });
 
 app.post('/api/courses', (req, res) => {
+    if (!req.body.name || req.body.name.length < 3 ) {
+        res.status(400).send('Course must have name property with a length of at least 3 characters');
+        return
+    }
     const course = {
         id: courses.length + 1,
         name: req.body.name
